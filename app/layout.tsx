3198c291b650cc4404036b9be9e4275c447902c1
brayan <brayan.challco.ticona2@gmail.com>
2026-08-01 18:51:31 -0400

    Agregar verificación Google Search Console

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,14 @@ const rubik = Rubik({
   display: "swap",
 });
 
-export const metadata: Metadata = {
-  title: "FLOR DE COCA",
-  description: "Alojamiento FLOR DE COCA",
-};
+export const metadata = {
+  title: "Alojamiento Flor de Coca",
+  description: "Alojamiento en La Paz, Bolivia.",
 
+  verification: {
+    google: "YmrQAKOvi-3ozcZlRuQIfwx2UoXNIF7-XMdhO48NWS8"
+  }
+};
 export default function RootLayout({
   children,
 }: Readonly<{
